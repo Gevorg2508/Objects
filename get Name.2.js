bcf2e9a6b54bf15781c1ff5de818490e4bac7d94
Harutyunyan Gevorg <gev.harutyunyan.96@gmail.com>
2021-12-14 21:51:10 +0400

    feat: nitral changes )

---
+++ get Name.2.js
@@ -0,0 +1,6 @@
+let person = { name: "Sarah", country: "Armenia", job: "Developer" };
+
+let {name, country, job} = person; 
+console.log(name); // "Sarah"
+console.log(country); // "Nigeria"
+console.log(job); // "Developer"
