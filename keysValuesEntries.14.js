d0e75f44b9492c5c21201ab6405fa4f6eed28cac
Harutyunyan Gevorg <gev.harutyunyan.96@gmail.com>
2021-12-15 08:40:44 +0400

    feat: keys, values, entries

---
+++ keysValuesEntries.14.js
@@ -0,0 +1,22 @@
+let obj = {
+    name: "John",
+    age: "30",
+    salary: 1000000,
+}
+let result = [];
+
+// for (let key in obj){
+//     result.push(key);
+// }
+// console.log(result);
+
+// for(let prop in obj){
+//  result.push(obj[prop]);
+// }
+// console.log(result);
+
+
+// for(let entries in obj){
+//     result.push(entries +":" + obj[entries])
+// }
+// console.log(result);
